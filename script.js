b062d555c21dac54071f6d2e24a445a75c59d237
roanblanchard <roan.g.blanchard@icloud.com>
2021-05-21 14:58:39 -0400

    number of days must be between 1 and 30

--- script.js
+++ script.js
@@ -5,16 +5,20 @@ let formIsValid
 
 const form = document.querySelector("#parking-form")
 const carYearInput = document.querySelector("#car-year")
+const daysInput = document.querySelector("#days")
 
 
 // runs all functions for validating the different fields
 form.addEventListener('submit', event => {
     event.preventDefault()
     validateCarYear()
+    validateDate()
 
     if (formIsValid) {
         // make the input label and border of the input green by adding input-valid on the parent div
-        document.querySelector("#car-year").classList.add('input-valid')
+        document.querySelector("#car-field").classList.add('input-valid')
+        document.querySelector(".input-group").classList.add('input-valid')
+        document.querySelector("#days-field").classList.add('input-valid')
     }
     // console.log('content inside email input', emailInput.value)
 })
@@ -35,15 +39,15 @@ function validateCarYear () {
 }
 
 // checks if the date is between 1 and 30
-// function validateCarYear () {
-//     if (carYearInput.value < 1900 ) {
-//         const error = document.createElement('div')
+function validateDate () {
+    if (daysInput.value < 1 || daysInput.value > 30) {
+        const error = document.createElement('div')
 
-//         // make the input label and border of the input red by adding input-valid on the parent div
-//         document.querySelector(".input-group").classList.add('input-invalid')
-//         // add an error message to the bottom of the input
-//         document.querySelector('#car-field').appendChild(error).innerHTML = 'Year of car must be later than 1900.'
-//     } else {
-//         formIsValid = true
-//     }
-// }
+        // make the input label and border of the input red by adding input-valid on the parent div
+        document.querySelector("#days-field").classList.add('input-invalid')
+        // add an error message to the bottom of the input
+        document.querySelector('#days-field').appendChild(error).innerHTML = 'Number of days must be between 1 and 30.'
+    } else {
+        formIsValid = true
+    }
+}
