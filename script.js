d785f37d71c2069d7dc19afe76346427e553a255
roanblanchard <roan.g.blanchard@icloud.com>
2021-05-21 14:50:51 -0400

    car year must be a number, car year must be after 1900, number of days must be a number

--- script.js
+++ script.js
@@ -1,1 +1,49 @@
-console.log('Add validation!');
+// console.log('Add validation!');
+
+// boolean for checking if the form is valid
+let formIsValid
+
+const form = document.querySelector("#parking-form")
+const carYearInput = document.querySelector("#car-year")
+
+
+// runs all functions for validating the different fields
+form.addEventListener('submit', event => {
+    event.preventDefault()
+    validateCarYear()
+
+    if (formIsValid) {
+        // make the input label and border of the input green by adding input-valid on the parent div
+        document.querySelector("#car-year").classList.add('input-valid')
+    }
+    // console.log('content inside email input', emailInput.value)
+})
+
+
+// checks if the car year is greater than 1900
+function validateCarYear () {
+    if (carYearInput.value < 1900 ) {
+        const error = document.createElement('div')
+
+        // make the input label and border of the input red by adding input-valid on the parent div
+        document.querySelector(".input-group").classList.add('input-invalid')
+        // add an error message to the bottom of the input
+        document.querySelector('#car-field').appendChild(error).innerHTML = 'Year of car must be later than 1900.'
+    } else {
+        formIsValid = true
+    }
+}
+
+// checks if the date is between 1 and 30
+// function validateCarYear () {
+//     if (carYearInput.value < 1900 ) {
+//         const error = document.createElement('div')
+
+//         // make the input label and border of the input red by adding input-valid on the parent div
+//         document.querySelector(".input-group").classList.add('input-invalid')
+//         // add an error message to the bottom of the input
+//         document.querySelector('#car-field').appendChild(error).innerHTML = 'Year of car must be later than 1900.'
+//     } else {
+//         formIsValid = true
+//     }
+// }
